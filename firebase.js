576b4009c349b20fd0f6b14238961edf535978b2
LONS <nn2577958@gmail.com>
2026-02-18 08:12:08 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
