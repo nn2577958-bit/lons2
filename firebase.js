f5eaf0b65eed16078427fc370d14f4198d880b64
LONS <nn2577958@gmail.com>
2026-02-18 04:07:25 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBeoiT7Bh0r83YY4Ga52auTmA5X92DMzKQ",
   authDomain: "lons2-74bfa.firebaseapp.com",
   projectId: "lons2-74bfa",
-  storageBucket: "lons2-74bfa.firebasestorage.app",
+  storageBucket: "lons2-74bfa.appspot.com",
   messagingSenderId: "734178165313",
-  appId: "1:734178165313:web:395ed759777ec61f4b3cc0",
-  measurementId: "G-LC2JJD493M"
+  appId: "1:734178165313:web:395ed759777ec61f4b3cc0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-console.log("Firebase Initialized", analytics);
+export const db = getFirestore(app);
+console.log("Firebase Initialized");
