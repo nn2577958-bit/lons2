a14cb4d55b6d33c716db690aa26cd90850e1daef
rlsnl <nn2577958@gmail.com>
2026-03-01 14:22:27 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyACDNgJpxqJM5P47c5D3HfIQIlpLfEjiKY",
   authDomain: "rani-mall-firebase.firebaseapp.com",
@@ -7,3 +12,8 @@ const firebaseConfig = {
   appId: "1:231199139441:web:fe2e971c00a8282cb95dd0",
   measurementId: "G-KYFWJED24P"
 };
+
+const app = initializeApp(firebaseConfig);
+
+// index.html에서 쓰려고 export
+export const auth = getAuth(app);
