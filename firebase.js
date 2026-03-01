e7c52586702d99da708a9c439b2a50d7368079bc
rlsnl <nn2577958@gmail.com>
2026-03-01 14:24:16 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyACDNgJpxqJM5P47c5D3HfIQIlpLfEjiKY",
@@ -10,10 +11,10 @@ const firebaseConfig = {
   storageBucket: "rani-mall-firebase.firebasestorage.app",
   messagingSenderId: "231199139441",
   appId: "1:231199139441:web:fe2e971c00a8282cb95dd0",
-  measurementId: "G-KYFWJED24P"
+  measurementId: "G-KYFWJED24P",
 };
 
 const app = initializeApp(firebaseConfig);
 
-// index.html에서 쓰려고 export
 export const auth = getAuth(app);
+export const db   = getFirestore(app);
