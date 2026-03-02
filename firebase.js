58ddb6e710efb7eb5bc5e5bfe34e138eab3981fc
rlsnl <nn2577958@gmail.com>
2026-03-02 11:25:30 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
 // firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
@@ -18,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
+export default app;
